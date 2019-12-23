e94e88b554b3a3b46ce0bbd638f41c4bddcfeba0
sumit3021kumar <manudogra3021@gmail.com>
2019-12-23 13:20:50 +0530

    initial commit

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Sumit Kumar.", "Designer.", "Developer.", "Freelancer."],
       typeSpeed: 100,
       loop: true,
     });
